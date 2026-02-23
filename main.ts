b3ac59ebd5b49927b171fffd1ca0e96612ca9d6f
Nomic-cat <124873135+Nomic-cat@users.noreply.github.com>
2026-02-23 17:13:19 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+    angle += 15
+    cuteBot.setServo(cuteBot.ServoList.S1, angle)
+    OLED.writeNumNewLine(angle)
+    if (angle == 180) {
+        cuteBot.setServo(cuteBot.ServoList.S1, 0)
+        angle = 0
+    }
+})
+let angle = 0
+cuteBot.setServo(cuteBot.ServoList.S1, 75)
+angle = 0
+OLED.init(128, 64)
+OLED.writeStringNewLine("prem A")
